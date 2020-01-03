f59ac71a85d2f32290c11c2980956116e7288b89
Vladislav Toigildin <vtoigildin@protonmail.com>
2020-01-03 18:38:28 +0300

    added info messages

--- torrentbot.py
+++ torrentbot.py
@@ -2,19 +2,6 @@
 # -*- coding: utf-8 -*-
 # This program is dedicated to the public domain under the CC0 license.
 
-"""
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
 import os
 import logging
 
@@ -31,20 +18,15 @@ logger = logging.getLogger(__name__)
 # context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+    update.message.reply_text('Привет! Я маленький войковский бот, который позволяет тебе удобно смотреть пиратский контент!')
 
 
 def help(update, context):
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+    update.message.reply_text('Просто вышли мне торрент файл, и я все сделаю!')
 
-
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
 
 def torrent_file(update, context):
-    """Echo the user message."""
     file_name = update.message.document.file_name
     if (file_name.endswith('.torrent')):
         update.message.reply_text(f'Я получил файл {file_name}. Начинаю загрузку.\
@@ -65,7 +47,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("1060437656:AAEgzTyzLmDELX3Df-CORDMwT1YaHVxEAyo", use_context=True)
+    updater = Updater(os.environ['TG_TOKEN'], use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
@@ -73,9 +55,6 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
 
     dp.add_handler(MessageHandler(Filters.document, torrent_file))
 
